525d163343fd330de443bc344493c33dd8891ce6
drei-6 <vendramin2k18@gmail.com>
2025-05-03 13:03:37 -0400

    programando 5

--- source/main.py
+++ source/main.py
@@ -1,3 +1,9 @@
+# render.txt
+"""
+type = emulator ; the video of the emulator
+type = rectangle, x = 0, y = 0, width = MAX_WIDTH, height = MAX_HEIGHT ; background
+"""
+
 import time
 import pygame
 
@@ -27,6 +33,10 @@ class RGBA:
 
 class Global:
     CHIP8_VIDEO_REFRESH_RATE = 60.0
+    CHIP8_STACK_DEPTH = 16
+    CHIP8_REGISTER_COUNT = 16
+    CHIP8_ENTRY_POINT_ADDRESS = 0x200
+    CHIP8_FONTSET_ADDRESS = 0x50
 
 class Button:
     is_down = False
@@ -46,9 +56,34 @@ class Input:
     mouse = Mouse()
     keyboard = Keyboard()
 
+class GUI:
+    pass
+
+class Render:
+    video = 0
+    input = 0
+    font = 0
+
+    def __init__(self, video: pygame.Surface, input: Input, font: pygame.font.Font):
+        self.video = video
+        self.input = input
+        self.font = font
+
+class Chip8:
+    memory = [0] * 4096
+    video = [0] * (64 * 32)
+    pc = 0
+    i = 0
+    stack = [0] * Global.CHIP8_STACK_DEPTH
+    delay_timer = 0
+    sound_timer = 0
+    register = [0] * Global.CHIP8_REGISTER_COUNT
+    opcode = 0
+
 class Emulator:
     fps = 0.0
     is_debugging = False
+    chip8 = Chip8()
 
 def log(string: str, *args: tuple):
     print(string % args)
@@ -82,13 +117,6 @@ def draw_character(video: pygame.Surface, font: pygame.font.Font, c: str, x: int
     result = surface_background.get_rect().width
     return result
 
-def draw_static_text(video: pygame.Surface, font: pygame.font.Font, text: str, x: int, y: int, max_width: int, max_height: int):
-    pass
-
-def draw_debugger(video: pygame.Surface, input:Input, font: pygame.font.Font, x: int, y: int):
-    if input.mouse.left_button.is_down:
-        draw_rectangle(video, Rectangle(0, 0, 500, 500), RGBA(1.0, 0.0, 0.0, 0.0))
-
 def draw_text(video: pygame.Surface, font: pygame.font.Font, text: str, x: int, y: int, is_anti_alising: bool, text_rgba: RGBA, background_rgba: RGBA):
     for c in text:
         if c == '\n':
@@ -97,11 +125,76 @@ def draw_text(video: pygame.Surface, font: pygame.font.Font, text: str, x: int, 
             continue
         x += draw_character(video, font, c, x, y, is_anti_alising, text_rgba, background_rgba)
 
+def draw_button(render: Render, x: int, y: int, text: str):
+    draw_rectangle(render.video, Rectangle(x, y, 100, 100), RGBA(1.0, 1.0, 1.0, 0.0))
+
+def draw_menu():
+    pass
+
+def draw_menubar():
+    pass
+
+def draw_gui(render: Render, gui: GUI):
+    draw_button(render, 0, 0, "Hello, world!!!")
+
+def update_gui(video: pygame.Surface, input: Input, font: pygame.font.Font, gui: GUI):
+    pass
+
 def emulator_initialize():
     result = Emulator()
     result.fps = Global.CHIP8_VIDEO_REFRESH_RATE
 
     return result
+
+def chip8_initialize(chip8: Chip8):
+    fontset = [
+        0xF0, 0x90, 0x90, 0x90, 0xF0,
+        0x20, 0x60, 0x20, 0x20, 0x70,
+        0xF0, 0x10, 0xF0, 0x80, 0xF0,
+        0xF0, 0x10, 0xF0, 0x10, 0xF0,
+        0x90, 0x90, 0xF0, 0x10, 0x10,
+        0xF0, 0x80, 0xF0, 0x10, 0xF0,
+        0xF0, 0x80, 0xF0, 0x90, 0xF0,
+        0xF0, 0x10, 0x20, 0x40, 0x40,
+        0xF0, 0x90, 0xF0, 0x90, 0xF0,
+        0xF0, 0x90, 0xF0, 0x10, 0xF0,
+        0xF0, 0x90, 0xF0, 0x90, 0x90,
+        0xE0, 0x90, 0xE0, 0x90, 0xE0,
+        0xF0, 0x80, 0x80, 0x80, 0xF0,
+        0xE0, 0x90, 0x90, 0x90, 0xE0,
+        0xF0, 0x80, 0xF0, 0x80, 0xF0,
+        0xF0, 0x80, 0xF0, 0x80, 0x80 
+    ]
+
+    for i in range(len(fontset)):
+        chip8.memory[Global.CHIP8_FONTSET_ADDRESS + i] = fontset[i]
+
+def chip8_load_rom(chip8: Chip8, file_name: str):
+    result = False
+
+    file = open(file_name, 'rb')
+    buffer = file.read()
+    file.close()
+
+    buffer_length = len(buffer)
+
+    if buffer_length > (len(chip8.memory) - Global.CHIP8_ENTRY_POINT_ADDRESS):
+        log("ROM size is too big: %u", buffer_length)
+        return result
+
+    for i in range(buffer_length):
+        chip8.memory[Global.CHIP8_ENTRY_POINT_ADDRESS + i] = buffer[i]
+
+    buffer = []
+
+    return True
+
+def chip8_cycle(chip8: Chip8):
+    # Fetch
+    # Decode and execute
+    # delay timer
+    # sound timer
+    pass
 
 def main():
     result = 1
@@ -128,10 +221,15 @@ def main():
         pygame.quit()
         return result
 
-    pygame.display.set_caption("Emulator")
-    
+    pygame.display.set_caption("Emulator")    
     emulator = emulator_initialize()
+    
+    chip8_initialize(emulator.chip8)
+    chip8_load_rom(emulator.chip8, "tetris.rom")
+
     input = Input()
+    render = Render(video, input, font)
+    gui = GUI()
     is_running = True
     last_counter = time.perf_counter_ns()
     
@@ -158,12 +256,15 @@ def main():
 
         if not is_running:
             break
+
+        # Update GUI
+        update_gui(video, input, font, gui)
         
         # Draw
         video.fill((0, 0, 0))
-        
-        if emulator.is_debugging or 1:
-            draw_debugger(video, input, font, 0, 0)
+
+        # Draw GUI
+        draw_gui(render, gui)
 
         # Timer
         work_counter = time.perf_counter_ns()
